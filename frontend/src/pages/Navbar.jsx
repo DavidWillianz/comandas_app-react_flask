bb0a665acf0200ef5df0dbad84b92366b5d72081
David <davidvieir22017@gmail.com>
2025-04-19 15:11:24 -0300

    navbar estilizado

--- frontend/src/pages/Navbar.jsx
+++ frontend/src/pages/Navbar.jsx
@@ -1,10 +1,20 @@
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
+import { AppBar, Toolbar, Typography, Button, IconButton, Box, useMediaQuery } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from "../context/AuthContext";
+import { useTheme } from '@mui/material/styles';
+
+import HomeIcon from '@mui/icons-material/Home';
+import PeopleIcon from '@mui/icons-material/People';
+import PersonIcon from '@mui/icons-material/Person';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Navbar = () => {
   const navigate = useNavigate();
   const { isAuthenticated, logout } = useAuth();
+
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down('sm'));
 
   const handleLogout = () => {
     logout();
@@ -33,44 +43,79 @@ const Navbar = () => {
 
         {isAuthenticated && (
           <Box display="flex" gap={1}>
-            <Button
-              color="inherit"
-              onClick={() => navigate('/home')}
-              sx={{ textTransform: 'none' }}
-            >
-              Home
-            </Button>
-            <Button
-              color="inherit"
-              onClick={() => navigate('/funcionarios')}
-              sx={{ textTransform: 'none' }}
-            >
-              Funcionários
-            </Button>
-            <Button
-              color="inherit"
-              onClick={() => navigate('/clientes')}
-              sx={{ textTransform: 'none' }}
-            >
-              Clientes
-            </Button>
-            <Button
-              color="inherit"
-              onClick={() => navigate('/produtos')}
-              sx={{ textTransform: 'none' }}
-            >
-              Produtos
-            </Button>
-            <Button
-              onClick={handleLogout}
-              sx={{
-                textTransform: 'none',
-                color: '#f87171',
-                fontWeight: 'bold',
-              }}
-            >
-              Sair
-            </Button>
+            {isSmallScreen ? (
+              <IconButton color="inherit" onClick={() => navigate('/home')}>
+                <HomeIcon />
+              </IconButton>
+            ) : (
+              <Button
+                color="inherit"
+                startIcon={<HomeIcon />}
+                onClick={() => navigate('/home')}
+                sx={{ textTransform: 'none' }}
+              >
+                Home
+              </Button>
+            )}
+            {isSmallScreen ? (
+              <IconButton color="inherit" onClick={() => navigate('/funcionarios')}>
+                <PeopleIcon />
+              </IconButton>
+            ) : (
+              <Button
+                color="inherit"
+                startIcon={<PeopleIcon />}
+                onClick={() => navigate('/funcionarios')}
+                sx={{ textTransform: 'none' }}
+              >
+                Funcionários
+              </Button>
+            )}
+            {isSmallScreen ? (
+              <IconButton color="inherit" onClick={() => navigate('/clientes')}>
+                <PersonIcon />
+              </IconButton>
+            ) : (
+              <Button
+                color="inherit"
+                startIcon={<PersonIcon />}
+                onClick={() => navigate('/clientes')}
+                sx={{ textTransform: 'none' }}
+              >
+                Clientes
+              </Button>
+            )}
+            {isSmallScreen ? (
+              <IconButton color="inherit" onClick={() => navigate('/produtos')}>
+                <ShoppingCartIcon />
+              </IconButton>
+            ) : (
+              <Button
+                color="inherit"
+                startIcon={<ShoppingCartIcon />}
+                onClick={() => navigate('/produtos')}
+                sx={{ textTransform: 'none' }}
+              >
+                Produtos
+              </Button>
+            )}
+            {isSmallScreen ? (
+              <IconButton color="inherit" onClick={handleLogout}>
+                <LogoutIcon />
+              </IconButton>
+            ) : (
+              <Button
+                onClick={handleLogout}
+                startIcon={<LogoutIcon />}
+                sx={{
+                  textTransform: 'none',
+                  color: '#f87171',
+                  fontWeight: 'bold',
+                }}
+              >
+                Sair
+              </Button>
+            )}
           </Box>
         )}
       </Toolbar>
